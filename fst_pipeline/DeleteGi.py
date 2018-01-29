0f092be6d49fc1e3ea6e7e5b81844d5d30347291
Johnjzy <jiangzhiyong12@yahoo.com>
2018-01-29 17:18:55 +0800

    Update DeleteGi.py

--- fst_pipeline/DeleteGi.py
+++ fst_pipeline/DeleteGi.py
@@ -1,2 +1,3 @@
+#the output of blastn may contain scafforlds, so we need to delete these lines include scafforlds
 with open('/disk-1/song/NHT130112/releaseData/re_call/sliding_win/DeleteGi.blast', 'w') as f:
     f.write(''.join([line for line in open('/disk-1/song/NHT130112/releaseData/re_call/sliding_win/12.fil.contigs.fa.blast').readlines() if 'gi' not in line]))
